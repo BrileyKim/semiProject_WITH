f90365925ad8a6d9194bd5ea77678f7ef861dd34
master <ganam@DESKTOP-JCVUR1A>
2020-09-17 21:32:40 +0900

    feat : add bidirectional encryptor and decryptor method
    Make logic and add this to gitignore. If someone see this logic, that
    might expose client's data

--- semiProject_WITH/src/com/with/common/AESCrypto.java
+++ semiProject_WITH/src/com/with/common/AESCrypto.java
@@ -1,5 +1,79 @@
 package com.with.common;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+// bidirectional encryptor object
+// Make sure control key!!
 public class AESCrypto {
 
+	private static SecretKey key;
+	private String path;
+	
+	public AESCrypto() {
+		this.path=AESCrypto.class.getResource("/").getPath();
+		File f = new File(this.path+"briley.br");
+		if(f.exists()) {
+			try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f))){
+				key=(SecretKey)(ois.readObject());
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			if(key==null) {
+				getGenerator();
+			}
+		}
+	}
+	
+	public void getGenerator() {
+		SecureRandom ser = new SecureRandom();
+		KeyGenerator keygen = null;
+		try {
+			keygen = KeyGenerator.getInstance("AES");
+			keygen.init(128,ser);
+			key=keygen.generateKey();
+		}catch(NoSuchAlgorithmException e){
+			e.printStackTrace();
+		}
+		File f = new File(this.path+"briley.br");
+		try(ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(f))){
+			oos.writeObject(this.key);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String encrypt(String str) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		byte[] encrypt=str.getBytes(Charset.forName("UTF-8"));
+		byte[] result = cipher.doFinal(encrypt);
+		String strResult = Base64.getEncoder().encodeToString(result);
+		return strResult;
+	}
+	
+	public static String decrypt(String str) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] decodeStr = Base64.getDecoder().decode(str.getBytes(Charset.forName("UTF-8")));
+		byte[] decrypt = cipher.doFinal(decodeStr);
+		return new String(decrypt);
+	}
 }
